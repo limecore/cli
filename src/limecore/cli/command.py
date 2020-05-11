2b75fc009f68c533506fbbca9403afd8dec146b7
astegosaurus <49028012+astegosaurus@users.noreply.github.com>
2020-05-11 11:02:27 +0100

    All arguments to be passed to Parsers

--- src/limecore/cli/command.py
+++ src/limecore/cli/command.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser, Namespace
 
 class Command:
     @classmethod
-    def create_argument_parser(cls, parser: ArgumentParser) -> None:
+    def create_argument_parser(cls, parser: ArgumentParser, **kwargs) -> None:
         pass
 
     def run(self, argv: Namespace):
